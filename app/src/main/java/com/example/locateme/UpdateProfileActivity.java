8b3bfb91b6dae2b6c1357faae4efdc46edc2aca9
dangphucthinh <49204195+dangphucthinh@users.noreply.github.com>
2019-11-11 15:32:28 +0700

    create chatroom

--- app/src/main/java/com/example/locateme/UpdateProfileActivity.java
+++ app/src/main/java/com/example/locateme/UpdateProfileActivity.java
@@ -5,8 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.locateme.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UpdateProfileActivity extends AppCompatActivity {
     private EditText mEdit_Phone;
@@ -14,23 +25,41 @@ public class UpdateProfileActivity extends AppCompatActivity {
     private EditText mEdit_Address;
     private Button mButton_Update;
     private Button mButton_Password;
+    private User currentUser;
+    String userId;
+    private String originalName;
+    private String originalPhone;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.profile_update);
         mEdit_Phone = findViewById(R.id.mEdit_Phone);
         mEdit_Name = findViewById(R.id.mEdit_Name);
         mEdit_Address = findViewById(R.id.mEdit_Address);
         mButton_Update = findViewById(R.id.btn_update);
-        mButton_Password = findViewById(R.id.btn_change_password);
+        mButton_Password = findViewById(R.id.btn_changePassword);
         setEvent();
+        loadData();
     }
     public void setmButton_Update() {
         //update message
+        String name = mEdit_Name.getText().toString();
+        String phone = mEdit_Phone.getText().toString();
+        if(name.equals("") && phone.equals("")){
+            Toast.makeText(this,"Please fill all the blank", Toast.LENGTH_LONG).show();
+        }
+        else if(originalPhone.equals(phone) && originalName.equals(name)) {
+                return;
+            }
+        else {
+            //update profile
+            finish();
+        }
+
     }
     public void setmButton_Password() {
         Intent intent = new Intent(this,ChangePasswordActivity.class);
-        startActivity(intent);
+        this.startActivity(intent);
     }
     public void setEvent() {
         mButton_Password.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +74,32 @@ public class UpdateProfileActivity extends AppCompatActivity {
                 setmButton_Update();
             }
         });
+    }
+
+    public void backButton(View v){
+        Intent intent = new Intent(this,ProfileActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void loadData() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            userId = intent.getStringExtra("id");
+            FirebaseDatabase.getInstance().getReference().child("users").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    currentUser = dataSnapshot.getValue(User.class);
+                    mEdit_Name.setText(currentUser.getName());
+                    mEdit_Phone.setText(currentUser.getPhone());
+                    originalName = currentUser.getName();
+                    originalPhone = currentUser.getPhone();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 }
